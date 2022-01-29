22419c584ad21ebfb76e05ca183940d4231e983e
abhinavsaxena99 <abhinav.saxena10@gmail.com>
2022-01-29 21:29:08 +0530

    Added UI

--- src/solver.py
+++ src/solver.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:gray'>Welcome!</h1>"
+def homepage():
+    return render_template('index.html', data = None)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    letters_present = request.form['letters_present']
+    letters_absent = request.form['letters_absent']
+    regex = request.form['regex']
+    print(letters_present,letters_absent,regex)
+
+
+    data = ["Mouse","Train","Solve","Right","Tests"]
+    return render_template('index.html', data = data)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
