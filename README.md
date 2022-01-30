5c095397dee0772eea725d0a0999229896d3c035
abhinavsaxena99 <abhinav.saxena10@gmail.com>
2022-01-30 13:50:23 +0530

    Fixed no module error

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 nginx + guincorn + flask(with jinja2)
 
+solver.service goes in /etc/systemd/system/solver.service
 solver.service allows controlling the wordle-solver service on the server
 For example, "sudo systemctl restart solver"
+To run:
+sudo systemctl enable solver
+sudo systemctl enable solver
+sudo systemctl status solver
 
 sites-enabled-solver goes in /etc/nginx/sites-enabled with the name "solver" and then is linked to sites-enabled
 using "sudo ln -s /etc/nginx/sites-available/solver /etc/nginx/sites-enabled"
--- src/solver.py
+++ src/solver.py
@@ -1,6 +1,36 @@
 from flask import Flask, send_from_directory, request, render_template
-import guess
+import pickle
 app = Flask(__name__)
+
+
+scored_words = {}
+ranked_words = []
+
+with open('src/ranked_words.pkl', 'rb') as f:
+    ranked_words = pickle.load(f)
+
+with open('src/scored_words.pkl', 'rb') as f:
+    scored_words = pickle.load(f)
+
+
+def find(letters_present, letters_absent, regex):
+
+    final_list = []
+    
+    
+    
+    # Basically when just 2 guesses are remaining, disregard word scores and recommend most common
+    # Can ask this from user or infer from length of list
+    if len(final_list) < 2:
+        pass
+    else:
+        final_list = sorted(final_list, key = lambda x: (scored_words[x],x), reverse = True)
+
+
+    if len(final_list) > 5:
+        return final_list[:5]
+    
+    return final_list
 
 @app.route("/")
 def homepage():
@@ -36,7 +66,7 @@ def my_form_post():
     
     # print(data, data["words"],len(data["words"]))
 
-    data["words"] = guess.find(letters_present,letters_absent,regex)
+    data["words"] = find(letters_present,letters_absent,regex)
     
     
     words_length = len(data["words"])
